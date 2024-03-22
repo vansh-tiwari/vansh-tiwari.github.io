e6a45db164f98b7309876491d526d6f4048cdb43
Vansh <tiwari1998@hotmail.com>
2024-03-22 11:46:51 +0530

    Added credits

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,21 +1,7 @@
-// export const Bio = {
+// export const CREDITS = {
 //   name: "Rishav Chanda",
-//   roles: [
-//     "Android",
-//     "Android Developer",
-//     "UI/UX Designer",
-//     "Programmer",
-//   ],
-//   description:
-//     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
 //   github: "https://github.com/rishavchanda",
-//   resume:
-//     "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-//   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-//   twitter: "https://twitter.com/RishavChanda",
-//   insta: "https://www.instagram.com/rishav_chanda/",
-//   facebook: "https://www.facebook.com/rishav.chanda.165/",
-// };
+// }
 export const Bio = {
   name: "Raghuvansh Mani Tiwari",
   roles: ["Javascript", "Angular", "React"],
